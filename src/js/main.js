04f955e938755d8373e0c3a7b34812c146f26d20
Graham Ballantyne <grahamb@sfu.ca>
2015-04-10 12:05:21 -0700

    require main.scss in dev
    Will be loaded normally through the canvas asset dingus in production

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 console.log('this is main.js');
 
+if (__DEV__) {
+  require('../scss/main.scss');
+}
+
 var React = require('react');
 console.log('main.js React', React.version);
 var Hello = React.createFactory(require('./components/Hello'));
