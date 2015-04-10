0d1341fe9446cf9a2977c8c54e36a1c1b75ef25a
Graham Ballantyne <grahamb@sfu.ca>
2015-04-10 13:45:56 -0700

    delete old require and gulp stuff

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
-
-gulp.task('webserver', function() {
-  gulp.src('www')
-    .pipe(webserver({
-      host: 'spaces.dev'
-  }));
-});
