5234eac33ae835931b934bd617be000f17c77440
Graham Ballantyne <grahamb@sfu.ca>
2015-04-10 12:02:35 -0700

    add sass loader for dev

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.js$/,
         loaders: ['react-hot', 'babel-loader'],
         include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loader: "style!css!sass"
       }
     ]
   },
