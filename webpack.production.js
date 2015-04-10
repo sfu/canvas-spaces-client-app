e5a264b221ed22bb6f6da8f469f21ed57d61f79f
Graham Ballantyne <grahamb@sfu.ca>
2015-04-10 11:13:25 -0700

    add production webpack config

---
+++ webpack.production.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var path = require('path');
+var Clean = require("clean-webpack-plugin");
+
+var pragmas = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
+});
+
+module.exports = {
+  entry: './src/js/main.js',
+
+  output: {
+    filename: 'canvas_spaces.js',
+    path: './dist'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    pragmas,
+    new Clean('dist')
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: [
+      __dirname + '/src/js',
+    ],
+    alias: {
+      'react': __dirname + '/vendor/canvas/public/javascripts/bower/react/react-with-addons',
+    }
+  },
+}
