9d68880f752a134777d46a8505ceaee9966c6c1e
Graham Ballantyne <grahamb@sfu.ca>
2015-04-10 12:05:35 -0700

    blippity-bloppity-boo

--- src/js/components/Hello.js
+++ src/js/components/Hello.js
@@ -5,16 +5,11 @@ const Hello = React.createClass({
     padding: '50px',
   },
 
-  devStuff() {
-    return __DEV__ ? <p>Development Mode</p> : '';
-  },
-
   render() {
     return (
       <div className="selector_that_should_not_appear_anywhere_else"style={this.divstyle}>
         <h1>Canvas Spaces</h1>
-        <p>HELLLLLLLLOOOOOOO... <World /></p>
-        {this.devStuff()}
+        <p>Hello <World /></p>
       </div>
     );
   }
