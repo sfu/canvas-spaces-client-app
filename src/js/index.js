c96af5a082c28a56f99f14ed692ebda32d83c672
Graham Ballantyne <grahamb@sfu.ca>
2015-04-13 15:49:35 -0700

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+
+import $ from 'jquery';
+$('h1').addClass('foo');
+
 if (__DEV__) {
   require('../scss/main.scss');
   window.React = window.React || React;
   console.log('React: ', React.version);
+  // console.log('jQuery:', $.fn.jquery);
 }
