4f2310b21e38b28ff0f9f3a05fdf155338f3d970
Graham Ballantyne <grahamb@sfu.ca>
2015-04-20 14:13:49 -0700

    add SpaceInitialUsersField component

---
+++ src/js/apps/CreateSpace/SpaceInitialUsersField.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const SpaceInitialUsersField = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+});
+
+export default SpaceInitialUsersField;
