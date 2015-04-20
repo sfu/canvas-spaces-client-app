05f6ff6be2970d50b99eda482336ddd4b840d083
Graham Ballantyne <grahamb@sfu.ca>
2015-04-20 10:01:14 -0700

    change GroupNameField to use ICInputField

--- src/js/apps/CreateSpace/GroupNameField.js
+++ src/js/apps/CreateSpace/GroupNameField.js
@@ -1,31 +1,22 @@
 import React from 'react';
-const Link = require('react-router').Link;
+import ICInputField from 'FormComponents/ICInputField';
 
 const GroupNameField = React.createClass({
 
-  getInitialState() {
-    return {
-      can_proceed: false
-    };
-  },
-
-  checkForDuplicateGroupName(evt) {
-    console.log('checkForDuplicateGroupName');
-    var group_name = this.refs.group_name.getDOMNode().value;
-    this.setState({
-      can_proceed: Boolean(group_name.length)
-    });
+  handleChange(event) {
+    this.props.update({name: this.refs.space_name.getValue()});
   },
 
   render() {
-    console.log(this.state);
-    console.log(this.props);
     return (
-      <div>
-        <label forName="group_name">Group Name</label>:&nbsp;
-        <input onKeyUp={this.checkForDuplicateGroupName} style={{'width': '300px'}} type="text" placeholder="Enter a name for your group" ref="group_name" autoFocus="true" defaultValue={this.props.data.group_name} />
-        <Link className="btn" disabled={!this.state.can_proceed} to="create_space_form" params={{step: 2}}>Continue</Link><br/>
-      </div>
+      <ICInputField
+        name="space_name"
+        label="Space Name"
+        placeholder="A short, descriptive name for your group (e.g. Basket Weaving Club)"
+        onChange={this.handleChange}
+        value={this.props.value}
+        ref="space_name"
+      />
     );
   }
 
