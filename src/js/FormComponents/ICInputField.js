a0be44cc4b30b5775e84129024c764574f035eb0
Graham Ballantyne <grahamb@sfu.ca>
2015-04-23 10:34:09 -0700

    allow passing errors to ICInputField
    rendered as a error popup, per the canvas styleguide
    
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- src/js/FormComponents/ICInputField.js
+++ src/js/FormComponents/ICInputField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 const {PropTypes} = React;
 
+const controlClass = 'ic-Form-control';
 const labelClass = 'ic-Label';
 const inputClass = 'ic-Input';
 
@@ -12,18 +13,35 @@ const ICInputField = React.createClass({
     value: PropTypes.string,
     placeholder: PropTypes.string,
     labelClasses: PropTypes.string,
-    inputClasses: PropTypes.string
+    inputClasses: PropTypes.string,
+    error: PropTypes.string
   },
 
   getValue() {
     return this.getDOMNode().querySelector('input[type="text"]').value;
   },
 
+  error() {
+    if (this.props.error) {
+      return (
+        <div className="ic-Form-message ic-Form-message--error">
+          <div className="ic-Form-message__Layout">
+            <i className="icon-warning" role="presentation"></i>
+              {this.props.error}
+          </div>
+        </div>
+      )
+    } else {
+      return null;
+    }
+  },
+
   render() {
     const labelClasses = this.props.labelClasses ? `${labelClass} ${this.props.labelClasses}` : labelClass;
     const inputClasses = this.props.inputClasses ? `${inputClass} ${this.props.inputClasses}` : inputClass;
+    const controlClasses = this.props.error ? `${controlClass} ${controlClass}--error` : controlClass;
     return (
-      <div className="ic-Form-control">
+      <div className={controlClasses}>
         <label
           htmlFor={this.props.name}
           className={labelClasses}>{this.props.label}
@@ -35,6 +53,7 @@ const ICInputField = React.createClass({
           placeholder={this.props.placeholder}
           onChange={this.props.onChange}
         />
+        {this.error()}
       </div>
     );
   }
