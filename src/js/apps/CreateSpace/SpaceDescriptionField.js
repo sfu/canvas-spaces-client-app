94d0d1cb361c4f0531922f2bda033df49d8b4e47
Graham Ballantyne <grahamb@sfu.ca>
2015-04-24 12:04:23 -0700

    add propTypes, errorLink and valueLink to SpaceDescriptionField
    Not very DRY, look at putting some of this stuff into a mixin?
    
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- src/js/apps/CreateSpace/SpaceDescriptionField.js
+++ src/js/apps/CreateSpace/SpaceDescriptionField.js
@@ -1,26 +1,85 @@
 import React from 'react';
+const {PropTypes} = React;
 import ICInputField from 'FormComponents/ICInputField';
 
-const GroupDescriptionField = React.createClass({
+const SpaceDescriptionField = React.createClass({
+
+  propTypes: {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    valueLink: PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      requestChange: PropTypes.func.isRequired
+    }).isRequired,
+    errorLink: PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      requestChange: PropTypes.func.isRequired
+    }).isRequired
+  },
+
+  getDefaultProps() {
+    return {
+      value: '',
+      error: '',
+      onChange: () => {},
+      valueLink: null,
+      errorLink: null
+    };
+  },
+
+  getValueLink(props) {
+    return props.valueLink || {
+      value: props.value,
+      requestChange: props.onChange
+    }
+  },
+
+  getErrorLink(props) {
+    return props.errorLink || {
+      value: props.value,
+      requestChange: this.setError
+    }
+  },
 
   handleChange(event) {
-    this.props.update({description: this.refs.space_description.getValue()});
+    const space_description = event.target.value;
+    this.clearError();
+    this.getValueLink(this.props).requestChange(space_description);
+  },
+
+  validate(event) {
+    const space_description = event.target.value;
+
+    // no empty descriptions
+    if (space_description === '') {
+      this.setError('You must enter a description for your Space');
+      return;
+    }
+  },
+
+  setError(error) {
+    this.getErrorLink(this.props).requestChange(error);
+  },
+
+  clearError() {
+    this.getErrorLink(this.props).requestChange('');
   },
 
   render() {
     return (
       <ICInputField
+        ref="space_description"
         name="space_description"
         label="Description"
         placeholder="A longer description of the purpose of your group"
         onChange={this.handleChange}
-        value={this.props.value}
-        ref="space_description"
-        {... this.props}
+        value={this.getValueLink(this.props).value}
+        error={this.getErrorLink(this.props).value}
+        onBlur={this.validate}
       />
     );
   }
 
 });
 
-export default GroupDescriptionField;
+export default SpaceDescriptionField;
