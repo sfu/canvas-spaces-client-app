73fdb1245d0783e23a642bac846a773f24f1382a
Graham Ballantyne <grahamb@sfu.ca>
2015-05-06 16:53:40 -0700

    initial MySpaces
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- src/js/apps/MySpaces/index.js
+++ src/js/apps/MySpaces/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import CommonHeader from 'apps/Shared/CommonHeader';
+import api from 'utils/api';
 
 const MySpaces = React.createClass({
+
+  getInitialState() {
+    return {
+      spaces: []
+    };
+  },
+
+  componentDidMount() {
+    api.get_spaces_for_user('self', (spaces) => {
+      this.setState({
+        spaces: spaces
+      })
+    }.bind(this))
+  },
 
   render() {
     return (
       <div>
-        <h2>My Spaces</h2>
+        <CommonHeader />
+        <div className="content-box">
+
+          <div className="grid-row">
+            <div className="col-xs">
+              <h2>My Canvas Spaces</h2>
+              <p>
+                These are the spaces of which you are a member.
+                Spaces of which you are the Leader are marked with a <i className="icon-star"></i>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-row">
+          <div className="col-xs">
+            <pre>{JSON.stringify(this.state, null, 2)}</pre>
+          </div>
+        </div>
+
       </div>
     );
   }
