545fa052665edca4b28ebc4a4c3eacd2966b6d0c
Graham Ballantyne <grahamb@sfu.ca>
2015-05-08 16:26:34 -0700

    add SpaceSettingsModal to SpaceTile
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- src/js/apps/Shared/SpaceTile/index.js
+++ src/js/apps/Shared/SpaceTile/index.js
@@ -5,6 +5,7 @@ const {PropTypes} = React;
 
 import SpaceTile_Information from './SpaceTile_Information';
 import SpaceTile_Avatar from './SpaceTile_Avatar';
+import SpaceSettingsModal from 'apps/Shared/SpaceSettingsModal';
 
 const SpaceTile = React.createClass({
 
@@ -16,6 +17,25 @@ const SpaceTile = React.createClass({
     space_id: PropTypes.number.isRequired
   },
 
+  getInitialState() {
+    return {
+      modalIsOpen: false
+    };
+  },
+
+  openModal(e) {
+    e.preventDefault();
+    this.setState({
+      modalIsOpen: true
+    });
+  },
+
+  closeModal() {
+    this.setState({
+      modalIsOpen: false
+    });
+  },
+
   render() {
     var space_url = `/groups/${this.props.space_id}`;
     if (__DEV__) {
@@ -23,18 +43,27 @@ const SpaceTile = React.createClass({
     }
 
     return (
-      <a href={space_url}>
-        <div className="SpaceTile">
-          <SpaceTile_Information
-            name={this.props.name}
-            description={this.props.description}
-            is_leader={this.props.is_leader}
-          />
-          <SpaceTile_Avatar
-            avatar={this.props.avatar}
-          />
-        </div>
-      </a>
+      <div>
+        <a href={space_url}>
+          <div className="SpaceTile">
+            <SpaceTile_Information
+              name={this.props.name}
+              description={this.props.description}
+              is_leader={this.props.is_leader}
+              editButtonHandler={this.openModal}
+            />
+            <SpaceTile_Avatar
+              avatar={this.props.avatar}
+            />
+          </div>
+        </a>
+        <SpaceSettingsModal
+          className="ReactModal__Content--canvas"
+          overlayClassName="ReactModal__Overlay--canvas"
+          modalIsOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+        />
+      </div>
     );
   }
 
