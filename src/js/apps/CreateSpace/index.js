ccea613ef8ce6dfe7171d287b7ad5760a624589b
Graham Ballantyne <grahamb@sfu.ca>
2015-05-11 12:15:25 -0700

    re-add disabled attribute to submit button
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- src/js/apps/CreateSpace/index.js
+++ src/js/apps/CreateSpace/index.js
@@ -121,7 +121,7 @@ const CreateSpace = React.createClass({
           <div className="ic-Form-actions">
             <button
               className="Button Button--primary"
-
+              disabled={this.state.disableSubmit}
               type="submit"
               onClick={this.handleSubmit}
           >
