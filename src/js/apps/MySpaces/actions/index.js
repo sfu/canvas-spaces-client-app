1370a2fcbd59b7d86b7ad821b2247e6145ba17d2
Graham Ballantyne <grahamb@sfu.ca>
2015-06-10 11:41:47 -0700

    add MySpaces actions
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

---
+++ src/js/apps/MySpaces/actions/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import alt from '../alt';
+import api from 'utils/api';
+
+class SpaceActions {
+  updateSpaces(spaces, links) {
+    this.dispatch({spaces, links});
+  }
+
+  fetchSpaces(next_link) {
+    this.dispatch();
+
+    if (!next_link) {
+      // initial load
+      api.get_spaces_for_user('self', (spaces, links) => {
+        this.actions.updateSpaces(spaces, links);
+      }, 3);
+    } else {
+      // load next pages
+      api.load_url(next_link, (spaces, links) => {
+        this.actions.updateSpaces(spaces, links);
+      });
+    }
+  }
+
+  spacesFailed(errorMessage) {
+    this.dispatch(errorMessage);
+  }
+}
+
+export default alt.createActions(SpaceActions);
