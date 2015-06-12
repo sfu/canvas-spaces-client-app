816fc9e01e22e72c8211ff3c735aa19342d93b7d
Graham Ballantyne <grahamb@sfu.ca>
2015-06-12 14:01:51 -0700

    Add ICSelect
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

---
+++ src/js/FormComponents/ICSelect.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import React from 'react';
+const {PropTypes} = React;
+
+const controlClass = 'ic-Form-control';
+const labelClass = 'ic-Label';
+const inputClass = 'ic-Input';
+
+const ICSelect = React.createClass({
+
+  propTypes: {
+    name: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    label: PropTypes.string.isRequired,
+    value: PropTypes.number,
+    placeholder: PropTypes.string,
+    labelClasses: PropTypes.string,
+    inputClasses: PropTypes.string,
+    error: PropTypes.string
+  },
+
+  getDefaultProps() {
+    return {
+      onBlur: () => {},
+      autoFocus: false,
+      options: []
+    };
+  },
+  getValue() {
+    return this.getDOMNode().querySelector('input[type="text"]').value;
+  },
+
+  options() {
+    return this.props.options.map((o, i) => {
+      return (
+        <option key={i} value={o.value}>{o.name}</option>
+      );
+    });
+  },
+
+  error() {
+    if (this.props.error) {
+      return (
+        <div className="ic-Form-message ic-Form-message--error">
+          <div className="ic-Form-message__Layout">
+            <i className="icon-warning" role="presentation"></i>
+              {this.props.error}
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  },
+
+  render() {
+    const labelClasses = this.props.labelClasses ? `${labelClass} ${this.props.labelClasses}` : labelClass;
+    const controlClasses = this.props.error ? `${controlClass} ${controlClass}--has-error` : controlClass;
+
+    const inputClasses = () => {
+      const baseClass = this.props.error ? `${inputClass} ${inputClass}--has-error` : inputClass;
+      return this.props.inputClasses ? `${baseClass} ${this.props.inputClasses}` : baseClass;
+    };
+
+    return (
+      <div className={controlClasses}>
+        <label
+          htmlFor={this.props.name}
+          className={labelClasses}>{this.props.label}
+        </label>
+        <select
+          id={this.props.name}
+          className={inputClasses()}
+          value={this.props.value}
+          onChange={this.props.onChange}
+          onBlur={this.props.onBlur}
+          autoFocus={this.props.autoFocus}
+          defaultValue={this.props.defaultValue}
+        >
+          {this.options()}
+        </select>
+        {this.error()}
+      </div>
+    );
+  }
+
+});
+
+export default ICSelect;
