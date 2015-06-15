73009083ec21848dc50310f774ca6537873bb5de
Graham Ballantyne <grahamb@sfu.ca>
2015-06-14 23:43:54 -0700

    remove console.log

--- canvas/public/javascripts/canvas_spaces.js
+++ canvas/public/javascripts/canvas_spaces.js
@@ -1,9 +1,8 @@
 // copy into public/javascripts/canvas_spaces.js
 require([ 'canvas_spaces' ], function(app) {
 
-  console.log('hello canvas_spaces');
 
 }, function(error) {
-  console.warn('CQS loading failed:', error, error.stack);
+  console.warn('Canvas Spaces loading failed:', error, error.stack);
 });
 
