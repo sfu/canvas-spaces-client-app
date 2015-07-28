73006ae6187c8f42da0135c1355f31b14508a049
Graham Ballantyne <grahamb@sfu.ca>
2015-07-28 11:05:18 -0700

    add wording for CommonHeader

--- src/js/Shared/CommonHeader.js
+++ src/js/Shared/CommonHeader.js
@@ -11,7 +11,15 @@ const CommonHeader = React.createClass({
       <div className="header-bar row-fluid">
         <div className="span8">
           <h1>Canvas Spaces</h1>
-          <p>Pop-up Austin gastropub Schlitz, health goth Bushwick mixtape semiotics you probably haven't heard of them four loko whatever. Cornhole single-origin coffee listicle, semiotics Carles meditation Kickstarter DIY food truck street art hella Echo Park Schlitz try-hard cliche. Actually readymade crucifix banh mi deep v gluten-free.</p>
+          <p>
+            Are you looking for a space to colloborate with your SFU club, research group, committee, or other group?
+            With Canvas Spaces, you can use Canvas features such as file sharing, discussions, and wiki pages, to bring
+            greater communication and collaboration to your group.
+          </p>
+          <p>
+            Canvas Spaces are currently restricted to uses with an @sfu.ca email address. For more information on Canvas Spaces,
+            please consult the <a href="#">documentation</a>.
+          </p>
         </div>
         <div className="span4 align-right">
           <Link to="create" className="icon-plus Button Button--primary"> Create New Space</Link>
