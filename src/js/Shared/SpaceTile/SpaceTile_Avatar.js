93ae1913a71bc0e43968de7bae028f84a4d636b2
Graham Ballantyne <grahamb@sfu.ca>
2015-11-25 17:02:42 -0800

    refactor SpaceTile_Avatar to a function component

--- src/js/Shared/SpaceTile/SpaceTile_Avatar.js
+++ src/js/Shared/SpaceTile/SpaceTile_Avatar.js
@@ -1,22 +1,14 @@
 'use strict';
 
 import React from 'react';
-const {PropTypes} = React;
+const {string} = React.PropTypes;
 
-const SpaceTile_Avatar = React.createClass({
-  propTypes: {
-    avatar: PropTypes.string.isRequired
-  },
+const SpaceTile_Avatar = ({avatar}) => (
+  <div className="SpaceTile--SpaceAvatar">
+    <img src={avatar} />
+  </div>
+);
 
-  render() {
-    return (
-      <div className="SpaceTile--SpaceAvatar">
-        <img src={this.props.avatar} />
-      </div>
-
-    );
-  }
-
-});
+SpaceTile_Avatar.propTypes = { avatar: string.isRequired };
 
 export default SpaceTile_Avatar;
