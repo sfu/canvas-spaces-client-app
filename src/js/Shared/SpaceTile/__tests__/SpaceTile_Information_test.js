d95f415c42dad9788c1e6da6aaed1ee5a301475e
Graham Ballantyne <grahamb@sfu.ca>
2015-11-25 17:02:17 -0800

    add test for SpaceTile_Information component

---
+++ src/js/Shared/SpaceTile/__tests__/SpaceTile_Information_test.js
@@ -0,0 +1,74 @@
+/* eslint-env mocha eslint-unused */
+'use strict';
+
+import React from 'react';
+import {createRenderer} from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJsx from 'expect-jsx';
+expect.extend(expectJsx);
+
+import SpaceTile_Information from '../SpaceTile_Information';
+
+before((done) => {
+  React; //bypass ESLint no-unused-var error
+  done();
+});
+
+describe('<SpaceTile_Information />', () => {
+
+  const oldError = console.error;
+  let renderer;
+
+  beforeEach(() => {
+    renderer = createRenderer();
+    console.error = (str) => {
+      throw new Error(str);
+    }
+  });
+
+  afterEach(() => {
+    console.error = oldError;
+  });
+
+  describe('when space leader', () => {
+    it('renders the element with the edit button and text', () => {
+      renderer.render(<SpaceTile_Information name="Test" description="Test" space_url="/test" is_leader={true} editButtonHandler={() => {}}/>);
+      expect(renderer.getRenderOutput()).toEqualJSX((
+        <div className="SpaceTile--SpaceInformation">
+          <a style={{color: '#000'}} href="/test"><h1 title="Test">Test</h1></a>
+          <h2 title="Test">Test</h2>
+          <button
+            className="SpaceTile--SpaceInformation-editButton Button Button--small"
+            onClick={() => {}}
+          >
+            <i className="icon-settings"></i>
+            Change Space Settings
+          </button>
+          <p className="SpaceTile--SpaceInformation-leaderNote">
+            You are the leader of this space.
+          </p>
+        </div>
+      ));
+    });
+  });
+
+  describe('when not space leader', () => {
+    it('renders the element without the edit button and text', () => {
+      renderer.render(<SpaceTile_Information name="Test" description="Test" space_url="/test" is_leader={false} editButtonHandler={() => {}} />);
+      expect(renderer.getRenderOutput()).toEqualJSX((
+        <div className="SpaceTile--SpaceInformation">
+          <a style={{color: '#000'}} href="/test"><h1 title="Test">Test</h1></a>
+          <h2 title="Test">Test</h2>
+          {''}
+        </div>
+      ));
+    });
+  });
+
+  it('throws when missing a required prop', () => {
+    expect(() => {
+      renderer.render(<SpaceTile_Information />)
+    }).toThrow(/Failed propType/);
+  });
+
+});
