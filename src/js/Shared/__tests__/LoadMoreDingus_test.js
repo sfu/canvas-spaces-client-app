f915a178a10fcec194e85e89b939a8501b054a76
Graham Ballantyne <grahamb@sfu.ca>
2015-12-04 11:58:23 -0800

    Add LoadMoreDingus tests

---
+++ src/js/Shared/__tests__/LoadMoreDingus_test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React from 'react';
+import {createRenderer} from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJsx from 'expect-jsx';
+expect.extend(expectJsx);
+
+import LoadMoreDingus from '../LoadMoreDingus';
+
+let renderer;
+
+describe('<LoadMoreDingus>', () => {
+  beforeEach(() => {
+    renderer = createRenderer();
+  });
+
+  it('renders with a static icon when not loading', () => {
+    renderer.render((
+      <LoadMoreDingus />
+    ));
+    console.log(renderer.getRenderOutput());
+  });
+
+  it('renders an empty div when no error is passed', () => {
+    renderer.render(<ErrorBox />);
+    expect(renderer.getRenderOutput()).toEqualJSX(<div />)
+  });
+});
